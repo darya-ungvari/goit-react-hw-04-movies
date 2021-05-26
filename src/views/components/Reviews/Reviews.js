66b580a85c4faf10a2aa7cf1848b32cf2edbb90c
darya-ungvari <63539808+darya-ungvari@users.noreply.github.com>
2021-05-26 23:07:51 +0300

    add reviews page

--- src/views/components/Reviews/Reviews.js
+++ src/views/components/Reviews/Reviews.js
@@ -1,12 +1,28 @@
 import { Component } from 'react';
-// import { POSTER_URL } from '../../../services/MoviesApi';
+import { POSTER_URL, fetchMovieReviews } from '../../../services/MoviesApi';
 import s from './Reviews.module.css';
 
 export default class Reviews extends Component {
+  state = { reviews: { results: [] } };
+  componentDidMount() {
+    fetchMovieReviews(this.props.match.params.movieId)
+      .then(request => this.setState({ reviews: request }))
+      .then(() => console.log(this.state.reviews));
+  }
   render() {
+    const reviews = this.state.reviews;
     return (
       <>
-        <h1>Reviews</h1>
+        <ul className={s.reviewList}>
+          {reviews.results.map(review => (
+            <li className={s.item}>
+              <p className={s.review}>
+                <span className={s.name}>{review.author}</span>
+                {review.content}
+              </p>
+            </li>
+          ))}
+        </ul>
       </>
     );
   }
